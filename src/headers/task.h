f29c7aa74fa15ca9504235f6ac4da16b503e24fc
ehh <kapoludek@gmail.com>
2018-04-21 21:47:34 +0200

    SleepTime added

--- src/headers/task.h
+++ src/headers/task.h
@@ -3,6 +3,7 @@
 
 #include "string.h"
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct Task
 {
@@ -15,5 +16,6 @@ typedef struct Task
 
 task* AddToEmpty(task*, int, int, char*, int);
 task* Add(task*, int, int, char*, int);
+int SleepTime(task*);
 
 #endif
--- src/task.c
+++ src/task.c
@@ -17,8 +17,8 @@ task* Add(task* first, int hours, int minutes, char* command, int info)
     if(first == NULL)
     {
         AddToEmpty(first, hours, minutes, command, info);
-    } 
-    else 
+    }
+    else
     {
         task *tmp, *p;
         p = first;
@@ -44,7 +44,19 @@ task* Add(task* first, int hours, int minutes, char* command, int info)
         tmp->info = info;
         tmp->next = p->next;
         p->next = tmp;
-
     }
     return first;
+}
+
+int SleepTime(task* task)
+{
+    double seconds;
+    time_t now;
+    struct tm* timeinfo;
+    time(&now);
+    timeinfo = localtime(&now);
+    timeinfo->tm_hour = task->hours;
+    timeinfo->tm_min = task->minutes;
+    seconds = difftime(mktime(timeinfo),now);
+    return ((int) seconds);
 }
